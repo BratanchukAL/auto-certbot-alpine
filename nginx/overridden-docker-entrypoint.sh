c481a79ba39028e59074acfad0ce800b0d0f9097
Alexey Bratanchuk <bratanchuk.al.work@gmail.com>
2025-04-07 19:44:51 +0300

    [upd] added --non-interactive

--- nginx/overridden-docker-entrypoint.sh
+++ nginx/overridden-docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -eu pipefail
 
 # parallel crond, certbot, nginx
     /usr/sbin/crond -f -l 8 -d 8 \
-&   certbot certonly --webroot --webroot-path=/var/www/certbot/ --email "${EMAIL}" --agree-tos --no-eff-email -d "${DOMAINS}" \
+&   certbot certonly --webroot --webroot-path=/var/www/certbot/ --email "${EMAIL}" --agree-tos --no-eff-email -d "${DOMAINS}" --non-interactive \
 &   sh /docker-entrypoint.sh nginx -g "daemon off;"
 
